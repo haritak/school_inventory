803866d815b9db7d8da9f91466808738c82312c8
Charitakis Yannis <github@ych.gr>
2017-06-19 19:49:05 +0300

    Added script to add most of the codes and descriptions of the 25062015 καταγραφή

---
+++ inventory/KT-25062015/store2db.rb
@@ -0,0 +1,36 @@
+#run this script with:
+#bin/rails runner path/to/filename.rb
+#
+#
+
+username = "haritak" 
+filename = "25_6_2015.inventory.txt"
+
+
+
+
+user = User.find_by(username: "haritak")
+
+if not user
+  puts "User #{username} was not found in database"
+end
+
+puts "This script will add to your database the codes and the descriptions found in #{filename}."
+puts "All changes to the database will be filled under #{username}."
+puts "Press enter to continue"
+gets
+
+File.new(filename).each do |line|
+  if line =~ /(ΚΤ-[0-9]+)\s+(.*)/
+    code = $1.strip
+    description = $2.strip
+    next if code=='' or description==''
+
+
+    Item.create( serial: code, description: description, user_id: user.id )
+    puts "#{code}: #{description} under user #{user.id} - #{user.username}"
+  end
+end
+
+puts "All done"
+
