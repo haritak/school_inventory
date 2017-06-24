24b4d55f7ca3652bd0a6f5ae1adab8051a3cd7f5
Charitakis Yannis <github@ych.gr>
2017-06-24 18:42:51 +0300

    Creating a new separate file which is then used to create the labels

--- etiketes/VP-etiketes/generate.VP.rb
+++ etiketes/VP-etiketes/generate.VP.rb
@@ -25,38 +25,8 @@ count=0
 #BOM: Byte Order Mark (UTF character)
 #if not specified, first labels looks as if it has an
 #extra space in front of it
-File.open("2017_06.ΚωδικοίΑιθουσών.txt", encoding:"bom|utf-8").each do |line|
-  line = line.strip
-
-  room_name = line[0, line.index(" ")]
-  puts ">#{room_name}<"
-
-  npcs = 1
-  nprinters = 0
-  if ["LAB1", "LAB2", "LAB3", "LAB4", "EN"].include?(room_name)
-    npcs = 11
-    places_name << room_name + "SERVER"
-    places_name << room_name + "RK1"
-    places_name << room_name + "RK2"
-    nprinters = 1
-  elsif ["A1", "A2"].include?(room_name)
-    npcs = 2
-    nprinters = 2
-  elsif [ "B1", "B2"].include?(room_name)
-    npcs = 5
-    nprinters = 2
-  elsif ["EN"].include?(room_name)
-    npcs = 8
-  end
-
-
-  npcs.times do |i|
-    places_name << room_name + sprintf("PC%d", i+1)
-  end
-  nprinters.times do |i|
-    places_name << room_name + sprintf("PR%d", i+1)
-  end
-  
+File.open("2017_06.VirtualPlaces.txt", encoding:"bom|utf-8").each do |line|
+  places_name << line.strip
   #break if (count+=1) > 2
 end
 
