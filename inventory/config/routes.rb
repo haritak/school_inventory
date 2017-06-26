27f73c5c3bf5d997a1f377294f535dd6e0203fb9
Charitakis Yannis <github@ych.gr>
2017-06-26 19:01:38 +0300

    Added routes for RM, I, and VP

--- inventory/config/routes.rb
+++ inventory/config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   get '/', to: 'items#index'
   get '/KT/:serial', to:'items#show' #KT in english letters
+  get '/RM/:serial', to:'items#show' #Rooms
+  get '/I/:serial', to:'items#show' #I english letters Items
+  get '/VP/:serial', to:'items#show' #I english letters Virtual Places
 
   get 'not_found', to: 'items#not_found'
   get 'picture', to: 'items#picture'
