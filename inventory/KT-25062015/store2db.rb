4494001b67d4c4b3dd09955749f5aa2ad8a1e7b2
Charitakis Yannis <github@ych.gr>
2017-06-29 21:37:39 +0300

    Fixed small bug in store2db that missed certain KT-codes

--- inventory/KT-25062015/store2db.rb
+++ inventory/KT-25062015/store2db.rb
@@ -17,11 +17,18 @@ puts "All changes to the database will be filled under #{username}."
 puts "Press enter to continue"
 gets
 
+unmatched_codes = {}
 File.new(filename).each do |line|
   if line =~ /(KT-[0-9]+)\s+(.*)/
     code = $1.strip
     description = $2.strip
-    next if code=='' or description==''
+    next if code==''
+    
+    if description==''
+      puts code
+      unmatched_codes[ code ] = ""
+      next
+    end
 
 
     begin
@@ -30,6 +37,36 @@ File.new(filename).each do |line|
     rescue ActiveRecord::RecordNotUnique => e
       puts "Serial #{code} already exists"
     end
+  end
+end
+
+start=-1
+code = ""
+File.new(filename).each_with_index do |line,i|
+  puts "#{i}:#{line}" if start>0
+  if line =~ /(KT-[0-9]+)\s+(.*)/
+    start = -1
+    code = $1.strip
+    if unmatched_codes.include?( code )
+      start = 1
+      puts "Looking for description of #{code}"
+    end
+  elsif start>0 and start<10
+    start+=1
+    line = line.strip
+    if line!='' and line!='1' and (not line.start_with?("Κτημ")) and (not line.start_with?("Περιγ"))
+      unmatched_codes[ code ] = line
+      puts "Found description of #{code} to be #{line}"
+      start=-1
+    end
+  end
+end
+
+unmatched_codes.each do |c, d| 
+  if d.strip != ''
+    puts "Matched code #{c} with #{d}"
+  else
+    puts "UNMATCHED CODE #{c}"
   end
 end
 
