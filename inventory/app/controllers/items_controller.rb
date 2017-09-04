9a0fb7b8b09b996d5214c5ee6b60eaf26a829e8b
Charitakis Yannis <github@ych.gr>
2017-09-04 19:08:33 +0300

    Bug: remove unecessary fields from correct params

--- inventory/app/controllers/items_controller.rb
+++ inventory/app/controllers/items_controller.rb
@@ -121,9 +121,9 @@ class ItemsController < ApplicationController
     @item.user_id = session[:user_id]
     @item.container_serial= params[:container_serial]
 
-    processed_params.delete :remove_photo
-    processed_params.delete :remove_second_photo
-    processed_params.delete :remove_invoice
+    item_params.delete :remove_photo
+    item_params.delete :remove_second_photo
+    item_params.delete :remove_invoice
 
     respond_to do |format|
       if @item.save
